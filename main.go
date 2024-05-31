9150e902769bf9b7383abb02351cdb66a01f1dab
root <root@LP1-AP-51897678.PROD.HCLPNP.COM>
2024-05-31 16:53:03 +0530

    checkin-8

--- main.go
+++ main.go
@@ -4,9 +4,15 @@ import "fmt"
 
 func main() {
     msg := sayHello("Alice")
+	msg2 := sayHello2("say hello 2")
     fmt.Println(msg)
+	fmt.Println(msg2)
 }
 
 func sayHello(name string) string {
+    return fmt.Sprintf("Hi %s", name)
+}
+
+func sayHello2(name string) string {
     return fmt.Sprintf("Hi %s", name)
 }

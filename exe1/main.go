16371b9c1bb9d36318f21d432e0c97b9ff212408
Isavara <isavaratraining@gmail.com>
2024-06-04 19:56:52 +0530

    fixed git version-1

--- exe1/main.go
+++ exe1/main.go
@@ -7,12 +7,9 @@ import (
 func main() {
 	fmt.Println("Hello, World!")
 	// Unused variable
-	var unusedVar string
 
 	// Ineffectual assignment
-	x := 42
-	x = 43
-	y = 90
+	var x = 42
 
 	// Redundant import
 	_ = fmt.Sprintf("This import is redundant")

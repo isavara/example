42b7515a4a33305b295a644926ddd4a2e60b1f86
Isavara <isavaratraining@gmail.com>
2024-06-05 12:57:23 +0530

    feature-bracnh-gitlint-test-2

--- exe2/main.go
+++ exe2/main.go
@@ -5,8 +5,8 @@ import "fmt"
 var unusedVar string
 
 func main() {
-	msg_var := sayHello("Alice")
-	fmt.Println(msg_var)
+	msg_var22 := sayHello("Alice")
+	fmt.Println(msg_var22)
 }
 
 func sayHello(name string) string {
